5bf287cb144efbe45456588063d3ff4742958682
Adam Grob <ad4mgr0b@gmail.com>
2017-11-20 23:30:18 +0100

    Removed the two lines of comment, renamed the variable frame to turn, as it is more understandable for me. Refactored the get_value function, removed 10 pieces of or statements.

--- bowling.py
+++ bowling.py
@@ -1,15 +1,13 @@
 def score(game):
     result = 0
-    frame = 1
+    turn = 1
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if turn < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -20,24 +18,20 @@ def score(game):
                     result += get_value(game[i+2])
         last = get_value(game[i])
         if not in_first_half:
-            frame += 1
+            turn += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
-            frame += 1
+            turn += 1
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in "123456789":
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char in "Xx/":
         return 10
     elif char == '-':
         return 0
