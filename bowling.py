34bd0713e83d55774542960c0c834069cec7285c
Adam Grob <ad4mgr0b@gmail.com>
2017-11-21 19:18:07 +0100

    New variable name:  max_points_per_try, is now contains the magic number 10 Some of the variable max_turns is now replaced with max_points_per_try. (They both have the same value) Added a main() function for better testing capabilities. And for debugging.

--- bowling.py
+++ bowling.py
@@ -4,19 +4,21 @@ def score(game):
     result = 0
     turn = 1
     max_turns = 10
+    max_points_per_try = 10
     first_try = True
     for i in range(len(game)):
         if game[i] is spare:
-            result += max_turns - last
+            result += max_points_per_try - last
         else:
             result += get_value(game[i])
-        if turn < max_turns and get_value(game[i]) == 10:
+        # This part executes unless it is the last turn
+        if turn < max_turns and get_value(game[i]) == max_points_per_try:
             if game[i] is spare:
                 result += get_value(game[i+1])
             elif game[i] in strike:
                 result += get_value(game[i+1])
                 if game[i+2] == spare:
-                    result += max_turns - get_value(game[i+1])
+                    result += max_points_per_try - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
@@ -39,3 +41,9 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+def main():
+    print(score("8/X47/xxxxx2451"))
+
+main()
