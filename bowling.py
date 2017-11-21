38f4588c50a648d130b5713854dedbe5b76352b3
Adam Grob <ad4mgr0b@gmail.com>
2017-11-21 14:43:02 +0100

    Variable in_first_half renamed to first_try. Characters "\" and "Xx" are now assigned to variable names spare and strike, this way it is more understandable what is happening in the code. Magic number 10 is assigned to the variable max turn. I made the True -false statement part a bit simpler, buy removing = True or = False from the conditional lines. String "123456789" is now replaced by the insnumeric()function.

--- bowling.py
+++ bowling.py
@@ -1,37 +1,41 @@
 def score(game):
+    spare = '/'
+    strike = "Xx"
     result = 0
     turn = 1
-    in_first_half = True
+    max_turns = 10
+    first_try = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+        if game[i] is spare:
+            result += max_turns - last
         else:
             result += get_value(game[i])
-        if turn < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+        if turn < max_turns and get_value(game[i]) == 10:
+            if game[i] is spare:
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i] in strike:
                 result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                if game[i+2] == spare:
+                    result += max_turns - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
-            turn += 1
-        if in_first_half == True:
-            in_first_half = False
+        if first_try:
+            first_try = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
+            first_try = True
+            turn += 1
+        if game[i] in strike:
+            first_try = True
             turn += 1
     return result
 
 def get_value(char):
-    if char in "123456789":
+    if char.isnumeric():
         return int(char)
     elif char in "Xx/":
         return 10
-    else:
+    elif char == '-':
         return 0
+    else:
+        raise ValueError()
