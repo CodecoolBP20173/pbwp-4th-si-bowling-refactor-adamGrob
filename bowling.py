eb32f50f1e3a870919036906b2a349c73220535e
Adam Grob <ad4mgr0b@gmail.com>
2017-11-22 12:16:28 +0100

    In get_value function,  changed the isnumeric  to the map buit in function. This makes the get value function more understandable. Also refactored the score function.

--- bowling.py
+++ bowling.py
@@ -7,33 +7,33 @@ def score(game):
     max_points_per_try = 10
     first_try = True
     for i in range(len(game)):
-        if game[i] is spare:
-            result += max_points_per_try - last
-        else:
-            result += get_value(game[i])
+        
         # This part executes unless it is the last turn
         if turn < max_turns and get_value(game[i]) == max_points_per_try:
-            if game[i] is spare:
-                result += get_value(game[i+1])
-            elif game[i] in strike:
-                result += get_value(game[i+1])
-                if game[i+2] == spare:
+            result += get_value(game[i+1])
+            if not (game[i] is spare): 
+                if game[i+2] is spare:
                     result += max_points_per_try - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(game[i])
-        if first_try:
-            first_try = False
+        # This parts executes at the last turns
+        if game[i] is spare:
+            result += max_points_per_try - get_value(game[i-1])
         else:
-            first_try = True
+            result += get_value(game[i])
+    
+        # This part decides if the turn is over.
+        if not first_try:
             turn += 1
+        first_try = not first_try
+
         if game[i] in strike:
             first_try = True
             turn += 1
     return result
 
 def get_value(char):
-    if char.isnumeric():
+    if char in map(str, range(1, 10)):
         return int(char)
     elif char in "Xx/":
         return 10
@@ -43,7 +43,8 @@ def get_value(char):
         raise ValueError()
 
 
-def main():
-    print(score("8/X47/xxxxx2451"))
 
-main()
+def main():
+    print(score("1/35XXX458/X3/XX6"))
+
+#main()
