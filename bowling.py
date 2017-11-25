555bd7a044cc48b92a5b47ab75b65ae5867bbe5d
Adam Grob <ad4mgr0b@gmail.com>
2017-11-25 17:12:58 +0100

    Extracted the spare  checking part of the score fucntion into a function called game_is_spare().

--- bowling.py
+++ bowling.py
@@ -7,30 +7,19 @@ def score(game):
     max_points_per_try = 10
     first_try = True
     for i in range(len(game)):
-        
-        # This part executes unless it is the last turn
         if turn < max_turns and get_value(game[i]) == max_points_per_try:
             result += get_value(game[i+1])
             if not (game[i] is spare): 
-                if game[i+2] is spare:
-                    result += max_points_per_try - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        # This parts executes at the last turns
-        if game[i] is spare:
-            result += max_points_per_try - get_value(game[i-1])
-        else:
-            result += get_value(game[i])
-    
-        # This part decides if the turn is over.
+                result = game_is_spare(i+2, game, spare, result, max_points_per_try)
+        result = game_is_spare(i, game, spare, result, max_points_per_try)
         if not first_try:
             turn += 1
         first_try = not first_try
-
         if game[i] in strike:
             first_try = True
             turn += 1
     return result
+
 
 def get_value(char):
     if char in map(str, range(1, 10)):
@@ -43,8 +32,14 @@ def get_value(char):
         raise ValueError()
 
 
+def game_is_spare(i, game, spare, result, max_points_per_try):
+    if game[i] is spare:
+        return result + max_points_per_try - get_value(game[i-1])
+    else:
+        return result + get_value(game[i])
+
 
 def main():
     print(score("1/35XXX458/X3/XX6"))
 
-#main()
+main()
