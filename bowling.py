db0f4df86d7cbf1bdf2694d0c097179960e8ff45
Adam Grob <ad4mgr0b@gmail.com>
2017-11-25 17:34:46 +0100

    Added function descriptions for all functions, and comments for hte score(game) function.

--- bowling.py
+++ bowling.py
@@ -1,4 +1,7 @@
 def score(game):
+    ''' Returns the score(number) of 10 rounds of bowling
+        from a string of characters.'''
+    # Variables
     spare = '/'
     strike = "Xx"
     result = 0
@@ -6,12 +9,14 @@ def score(game):
     max_turns = 10
     max_points_per_try = 10
     first_try = True
+    # Scoring
     for i in range(len(game)):
         if turn < max_turns and get_value(game[i]) == max_points_per_try:
             result += get_value(game[i+1])
             if not (game[i] is spare): 
                 result = game_is_spare(i+2, game, spare, result, max_points_per_try)
         result = game_is_spare(i, game, spare, result, max_points_per_try)
+        # Decides if it is the first throw or the second.
         if not first_try:
             turn += 1
         first_try = not first_try
@@ -22,6 +27,9 @@ def score(game):
 
 
 def get_value(char):
+    ''' Returns the corresponding number of the
+        character.
+        Arg = string. Returns = number. '''
     if char in map(str, range(1, 10)):
         return int(char)
     elif char in "Xx/":
@@ -33,6 +41,8 @@ def get_value(char):
 
 
 def game_is_spare(i, game, spare, result, max_points_per_try):
+    ''' Returns the corresponding result if the game is spare.
+        Returns a number. '''
     if game[i] is spare:
         return result + max_points_per_try - get_value(game[i-1])
     else:
